2d14f1ea4ceed12746d9a6e5194591d585d6c3f5
KrishnanditoLksn <krislaksana123@gmail.com>
2023-07-03 12:13:17 +0700

    Separating all files based on programming languagues that i i've use

---
+++ HackerRank/JAVA/loops2.java
@@ -0,0 +1,28 @@
+package HackerRank;
+
+import javax.crypto.MacSpi;
+import java.util.Scanner;
+
+public class loops2 {
+    public static void main(String[] args) {
+        Scanner robot = new Scanner(System.in);
+        int q = robot.nextInt();
+
+        for (int i = 0; i < q; i++) {
+            int a = robot.nextInt();
+            int b = robot.nextInt();
+            int c = robot.nextInt();
+            double p;
+            for (int j = 0; j < c; j++) {
+
+            /*for (int j = 0;  j  < c; j++){
+                p = Math.pow(2 , j);
+                int p1 = (int)p;
+                a = a + (p1 * b);
+                System.out.println(a + " ");
+            }
+            System.out.println();*/
+            }
+        }
+    }
+}
---
+++ HackerRank/JAVA/stdout.java
@@ -0,0 +1,15 @@
+package HackerRank;
+
+import java.util.Scanner;
+
+public class stdout {
+    public static void main(String[] args) {
+        Scanner robot = new Scanner(System.in);
+        int a = robot.nextInt();
+        for (int i = 1 ; i < a + 1; i++) {
+            System.out.println(i);
+        }
+    }
+
+
+}
