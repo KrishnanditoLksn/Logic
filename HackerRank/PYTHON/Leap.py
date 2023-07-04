65865f30130073048e12031456620b4df901039e
KrishnanditoLksn <krislaksana123@gmail.com>
2023-07-04 10:42:39 +0700

    hackerrank

---
+++ HackerRank/PYTHON/Leap.py
@@ -0,0 +1,14 @@
+def is_Leap(year ):
+    leap = False
+
+    if year % 4 == 0 and  (year % 100 != 0 or  year % 400 == 0):
+        return True
+    
+    
+    return leap
+
+
+
+
+year = int(input())
+print(is_Leap(year))
---
+++ HackerRank/PYTHON/split_join.py
@@ -0,0 +1,4 @@
+def split_and_join(line):
+    line  = (input())
+    line = line.split(" ")
+    print(line)
