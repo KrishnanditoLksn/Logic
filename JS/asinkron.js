cbc30567a24f8271d48ecea66432f9d32f0b1121
KrishnanditoLksn <krislaksana123@gmail.com>
2023-08-26 22:11:53 +0700

    added json to import module

--- JS/asinkron.js
+++ JS/asinkron.js
@@ -1,21 +1,59 @@
-console.log("welcome")
+// console.log("welcome")
 
-setTimeout(()=>{
-    console.log("hufttt why it take too long!!")
-}, 3000)
+// setTimeout(()=>{
+//     console.log("hufttt why it take too long!!")
+// }, 3000)
 
-console.log("Arigatouu!!!")
+// console.log("Arigatouu!!!")
 
-function getUsers(callback) {
-    // simulate network delay
-    setTimeout(() => {
-        const users = ['John', 'Jack', 'Abigail'];
-        callback(users);
-    }, 3000);
+// function getUsers(callback) {
+//     // simulate network delay
+//     setTimeout(() => {
+//         const users = ['John', 'Jack', 'Abigail'];
+//         callback(users);
+//     }, 3000);
+// }
+
+// function usersCallback(users) {
+//     console.log(users);
+// }
+
+// getUsers(usersCallback)
+
+// function getUsers(isOffline, callback) {
+//     // simulate network delay
+//     setTimeout(() => {
+//         const users = ['John', 'Jack', 'Abigail'];
+
+//         if (isOffline) {
+//         callback(new Error('cannot retrieve users due offline'), null);
+//         return;
+// }
+
+//     callback(null, users);
+// }, 3000);
+// }
+
+// function usersCallback(error, users) {
+//     if (error) {
+//         console.log('process failed:', error.message);
+//     return;
+//     }
+//     console.log('process success:', users);
+// }
+// getUsers(false, usersCallback); // process success: ['John', 'Jack', 'Abigail']
+// getUsers(true, usersCallback); // process failed: cannot retrieve users due offline
+
+function getUserWeather(userId) {
+    try {
+        const user = getUser(userId);
+        const weather = getWeather(user.location);
+        return { ...user, ...weather };
+    } catch (error) {
+        console.log(error.message);
+        return null;
+    }
 }
 
-function usersCallback(users) {
-    console.log(users);
-}
-
-getUsers(usersCallback)
+const userWeather = getUserWeather(1);
+console.log(userWeather);
