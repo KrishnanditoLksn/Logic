21d2928679e93b7f50e321bbf0e4c9e9090ab85f
KrishnanditoLksn <krislaksana123@gmail.com>
2023-10-09 07:28:27 +0700

    added 1 exercise

---
+++ CodeChef/0-100*Beginner/course_regis.java
@@ -0,0 +1,5 @@
+public class course_regis{
+    public static void main(String[] args) {
+        System.out.println(" f");
+    }
+}
---
+++ CodeChef/practice_perfect.java
@@ -0,0 +1,21 @@
+package CodeChef;
+
+import java.util.*;
+
+public class practice_perfect {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            int[] dayArr;
+            int week = 0;
+            for (int i = 0; i < 4; i++) {
+                dayArr = new int[4];
+                dayArr[i] = input.nextInt();
+
+                if (dayArr[i] >= 10) {
+                    week += 1;
+                }
+            }
+            System.out.println(week);
+        }
+    }
+}
