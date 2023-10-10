73998addfe5bcbc8fa764ac85874816683506320
KrishnanditoLksn <krislaksana123@gmail.com>
2023-10-11 05:44:12 +0700

    added more exercises

---
+++ CodeChef/airlines.java
@@ -0,0 +1,21 @@
+package CodeChef;
+
+import java.util.Scanner;
+
+public class airlines {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            int n = input.nextInt();
+            for (int i = 0; i < n; i++) {
+                int eachAirplaneCapacity = 10 * input.nextInt();
+                int nPlanePeople = input.nextInt();
+                
+                int max = Math.min(eachAirplaneCapacity, nPlanePeople);
+
+                int seatCose = max * input.nextInt();
+                System.out.println(seatCose);
+                
+            }
+        }
+    }
+}
